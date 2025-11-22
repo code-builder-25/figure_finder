b2b3ed1b5fea6febbcec258269a3fdd69cfba464
code-builder-25 <code_builder@abv.bg>
2025-11-22 18:43:14 +0200

    chore: Rename components

--- src/finders/figure_finder_gcc.cpp
+++ src/finders/figure_finder_gcc.cpp
@@ -12,26 +12,26 @@ size_t FigureFinderGCC::countFigures(const BoolMatrix& matrix) {
 
   // Convert 2D coordinates to 1D index
   auto to_index = [cols](size_t row, size_t col) -> size_t { return row * cols + col; };
-  ConnectedComponents cc(rows * cols);
+  ConnectedComponents components(rows * cols);
 
   for (size_t row = 0; row < rows; ++row) {
     for (size_t col = 0; col < cols; ++col) {
       if (matrix(row, col)) {
         size_t current_idx = to_index(row, col);
-        cc.addNode(current_idx);
+        components.addNode(current_idx);
 
         // Connect to left and top neighbors if they exist
         if (col > 0 && matrix(row, col - 1)) {
-          cc.connect(current_idx, to_index(row, col - 1));
+          components.connect(current_idx, to_index(row, col - 1));
         }
         if (row > 0 && matrix(row - 1, col)) {
-          cc.connect(current_idx, to_index(row - 1, col));
+          components.connect(current_idx, to_index(row - 1, col));
         }
       }
     }
   }
 
-  return cc.getCount();
+  return components.getCount();
 }
 
 } // namespace figures
